e67c5be6ba179d55e61395f46bea615008d2cb81
paspf <pascal.pfeiffer@zeiss.com>
2024-11-12 16:17:19 +0100

    Add device selection

--- demos/live_demo.py
+++ demos/live_demo.py
@@ -15,17 +15,17 @@ parser.add_argument("--checkpoint", type=str, default="sam2_checkpoints/sam2.1_h
 parser.add_argument('--video', default=0, help='Path to a video or a camera id, default: 0')
 args = parser.parse_args()
 
-# use bfloat16 for the entire notebook
-torch.autocast(device_type="cuda", dtype=torch.bfloat16).__enter__()
+device = "cuda"
+if device == "cuda":
+    # use bfloat16 for the entire notebook
+    torch.autocast(device_type="cuda", dtype=torch.bfloat16).__enter__()
 
-if torch.cuda.get_device_properties(0).major >= 8:
-    # turn on tfloat32 for Ampere GPUs (https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices)
-    torch.backends.cuda.matmul.allow_tf32 = True
-    torch.backends.cudnn.allow_tf32 = True
+    if torch.cuda.get_device_properties(0).major >= 8:
+        # turn on tfloat32 for Ampere GPUs (https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices)
+        torch.backends.cuda.matmul.allow_tf32 = True
+        torch.backends.cudnn.allow_tf32 = True
 
-
-
-predictor = build_sam2_camera_predictor(args.config, args.checkpoint)
+predictor = build_sam2_camera_predictor(args.config, args.checkpoint, device=torch.device(device))
 
 
 cap = cv2.VideoCapture(args.video)
