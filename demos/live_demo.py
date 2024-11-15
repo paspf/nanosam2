ab9ff70b881c1c1ce02f61a7bd09c8fff945ee7e
paspf <pascal.pfeiffer@zeiss.com>
2024-11-15 16:01:00 +0100

    Print inference time and compile model.

--- demos/live_demo.py
+++ demos/live_demo.py
@@ -7,7 +7,7 @@ import numpy as np
 import cv2
 import argparse
 from nanosam2.sam2.build_sam import build_sam2_camera_predictor
-
+import time
 
 parser = argparse.ArgumentParser(description='Run benchmarks on different devices with different model optimizations.')
 parser.add_argument("--config", type=str, default="sam2_hiera_s", help="The path to a sam2 config.")
@@ -27,6 +27,25 @@ if device == "cuda":
 
 predictor = build_sam2_camera_predictor(args.config, args.checkpoint, device=torch.device(device))
 
+
+def _compile_model_blocks(model, model_settings:list, compile_backend):
+    print("Compiling Model...")
+    if model_settings[0]: # image_encoder
+        model.image_encoder = torch.compile(model.image_encoder, backend=compile_backend, dynamic=False)
+    if model_settings[1]: # memory_attention
+        model.memory_attention = torch.compile(model.memory_attention, backend=compile_backend)
+    if model_settings[2]: # sam_mask_decoder
+        model.sam_mask_decoder = torch.compile(model.sam_mask_decoder, backend=compile_backend)
+    if model_settings[3]: # sam_prompt_encoder
+        model.sam_prompt_encoder = torch.compile(model.sam_prompt_encoder, backend=compile_backend)
+    if model_settings[4]: # memory_encoder
+        model.memory_encoder = torch.compile(model.memory_encoder, backend=compile_backend)
+
+    return model
+
+
+predictor = _compile_model_blocks(predictor, [True, True, False, False, True], "inductor")
+print("Compile finished.")
 
 cap = cv2.VideoCapture(args.video)
 
@@ -75,8 +94,9 @@ while True:
         # )
 
     else:
+        start = time.perf_counter()
         out_obj_ids, out_mask_logits = predictor.track(frame)
-
+        mid = time.perf_counter()
         all_mask = np.zeros((height, width, 1), dtype=np.uint8)
         # print(all_mask.shape)
         for i in range(0, len(out_obj_ids)):
@@ -88,6 +108,10 @@ while True:
 
         all_mask = cv2.cvtColor(all_mask, cv2.COLOR_GRAY2RGB)
         frame = cv2.addWeighted(frame, 1, all_mask, 0.5, 0)
+        end = time.perf_counter()
+        prediction_time = (mid-start)
+        drawing_time = (end-mid)
+        print(f"prediction_time: {prediction_time} | drawing time: {drawing_time}")
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     cv2.imshow("frame", frame)
 
